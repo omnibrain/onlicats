7434b39889a76804da67640945045e6ac49d630d
Raphael Voellmy <r.voellmy@gmail.com>
2022-02-12 21:29:09 +0100

    feat(setup): basic next-auth setup with prisma provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,8 @@ import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
